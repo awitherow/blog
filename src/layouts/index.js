1b10ab916720f976cf87572b0448d30aacfedbee
awitherow <au.witherow@gmail.com>
2017-07-10 22:34:04 +0200

    update design and make placeholder

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,61 +1,67 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
-import { rhythm } from "../utils/typography"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+import { rhythm } from "../utils/typography";
 
-export default class Template extends React.Component {
+export default class Home extends Component {
   static propTypes = {
-    children: PropTypes.func,
-  }
+    children: PropTypes.func
+  };
 
   render() {
     return (
       <div>
-        <Helmet
-          title="Gatsby Default Starter"
-          meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
-          ]}
-        />
-        <div
-          style={{
-            background: `rebeccapurple`,
-            marginBottom: rhythm(1),
-          }}
-        >
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
-            }}
-          >
+        <Helmet title={meta.title} meta={meta.tags} />
+        <div style={styles.container}>
+          <div style={styles.header}>
             <h1 style={{ margin: 0 }}>
-              <Link
-                to="/"
-                style={{
-                  color: "white",
-                  textDecoration: "none",
-                }}
-              >
-                Gatsby
+              <Link to="/" style={styles.link}>
+                Home
               </Link>
             </h1>
           </div>
         </div>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
-            paddingTop: 0,
-          }}
-        >
+        <div style={styles.children}>
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
+
+const styles = {
+  container: {
+    background: `#f9f9f9`,
+    marginBottom: rhythm(1)
+  },
+  header: {
+    margin: `0 auto`,
+    maxWidth: 960,
+    padding: `${rhythm(1)} ${rhythm(3 / 4)}`
+  },
+  children: {
+    margin: `0 auto`,
+    maxWidth: 960,
+    padding: `${rhythm(1)} ${rhythm(3 / 4)}`,
+    paddingTop: 0
+  },
+  link: {
+    color: "inherit",
+    textDecoration: "none"
+  }
+};
+
+const meta = {
+  title: "Thoughts and Things",
+  tags: [
+    {
+      name: "description",
+      content: "Austin Witherows blog where he write about thoughts and things."
+    },
+    {
+      name: "keywords",
+      content: "programming, art, martial arts, solidarity"
+    }
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
-import React from "react"
-import Link from "gatsby-link"
-import Helmet from "react-helmet"
+import React, { Component } from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-export default class Index extends React.Component {
+export default class Index extends Component {
   render() {
     return (
       <div>
         <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+        <p>
+          The blog is underway, don't worry. Read more at{" "}
+          <a href="https://medium.com/@azurikai">Medium</a> for the meantime!
+        </p>
       </div>
-    )
+    );
   }
 }
